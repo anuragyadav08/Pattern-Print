2b996b572c3b3fd7150b45d1cb0f2bda3bb6f6dd
Anurag Yadav <101646300+anuragyadav08@users.noreply.github.com>
2022-03-31 18:23:15 +0530

    Add files via upload

---
+++ PATTERN2.C
@@ -0,0 +1,20 @@
+//reverse triangle  pattern print
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+clrscr();
+printf("Enter No. of Row :");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+  for(j=1;j<=i;j++)
+  {
+   printf("* ");
+  }
+  printf("\n");
+}
+getch();
+
+}
---
+++ PATTERN3.C
@@ -0,0 +1,24 @@
+//triangle 2 pattern print
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,k,n;
+clrscr();
+printf("Enter No. of Row :");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+for(k=i-1;k>0;k--)
+ {
+  printf(" ");
+ }
+  for(j=1;j<=n-i+1;j++)
+  {
+   printf("*");
+  }
+  printf("\n");
+}
+getch();
+
+}
